b6afe4d2444a0fd6376d831692935d3fd665a098
Shinie Aggarwal <shinieaggarwal72@gmail.com>
2024-05-01 23:04:17 +0530

    Update leastCostMethod.m

--- leastCostMethod.m
+++ leastCostMethod.m
@@ -1,10 +1,26 @@
 clc
 clear all
+
+%% Example problem:
+% cost matrix:
+%   2   10  4   5
+%   6   12  8   11
+%   3   9   5   7
+
+% avalibility:
+%   12  25  20
+
+% demand:
+%   25  10  15  5
+
+%% input 
 c = [2 10 4 5; 6 12 8 11; 3 9 5 7];
 [m,n] = size(c);
 a = [12 25 20];
 b = [25 10 15 5];
 z = 0;
+
+%% Make cost matrix balanced
 if sum(a)==sum(b)
     fprintf('Given transportation problem is balanced\n');
 else
@@ -17,6 +33,8 @@ else
         b(end+1) = sum(a) - sum(b);
     end
 end
+
+%% Loop until all supply or demand is exhausted
 x = zeros(m,n);
 initialC = c;
 for i = 1: m
@@ -41,11 +59,13 @@ for i = 1: m
     end
 end
 
+%% Compute the transportation cost
 for i = 1:m
     for j = 1:n
         z = z + initialC(i,j)*x(i,j);
     end
 end
 
+%% Display
 array2table(x);
 fprintf('Transportation cost = %d', z);
